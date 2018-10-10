5bf6d7e2b9e829cd3d9af08ffb8483e495ddebb6
J Guthrie <jamie.guthrie@gmail.com>
2018-10-10 23:54:23 +0100

    Completed Day 4

---
+++ day4-valid_passphrases-a.rb
@@ -0,0 +1,16 @@
+total = 0
+
+File.open('day4-valid_passphrases.txt').each do |line|
+  phrases = line.split(/\s+/).sort
+
+  while phrases.length > 0
+    if phrases.pop == phrases.last
+      total -= 1
+      break
+    end
+  end
+
+  total += 1
+end
+
+puts "Solution: #{total}"
